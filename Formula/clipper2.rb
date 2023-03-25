0a8bffbab0d0375fc6bdd255be143d0eba7514ad
Pavel Zwerschke <pavelzw@gmail.com>
2023-03-25 19:38:00 +0100

    test clipper true + readme

--- Formula/clipper2.rb
+++ Formula/clipper2.rb
@@ -32,6 +32,7 @@ class Clipper2 < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
+    # system "false"
   end
 end
