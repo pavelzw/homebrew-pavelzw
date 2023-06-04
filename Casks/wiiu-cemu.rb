1135585e8b998e0ec1065ad4dc6a7028b2c609a8
Pavel Zwerschke <pavelzw@gmail.com>
2023-06-04 13:25:55 +0200

    wiiu-cemu (2.0-39)

--- Casks/wiiu-cemu.rb
+++ Casks/wiiu-cemu.rb
@@ -2,8 +2,8 @@
 # https://docs.brew.sh/How-to-Create-and-Maintain-a-Tap#naming
 
 cask "wiiu-cemu" do
-  version "2.0-36"
-  sha256 "4db5bce074688f2c5706c466bc93b0b51f99f1cd967aa2158834d2cb1fad6d64"
+  version "2.0-39"
+  sha256 "353bfa8508b6362063d9d6fe898b558bb739efd20ae7f4a50853eb6ec2fdf89a"
 
   url "https://github.com/cemu-project/Cemu/releases/download/v#{version}/cemu-#{version}-macos-12-x64.dmg",
       verified: "github.com/cemu-project/Cemu/"
