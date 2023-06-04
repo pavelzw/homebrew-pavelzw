eb74fa9aa7c4b7c998dbddb254e6d1a893994bd0
Pavel Zwerschke <pavelzw@gmail.com>
2023-06-04 13:24:36 +0200

    upscayl (2.5.5)

--- Casks/upscayl.rb
+++ Casks/upscayl.rb
@@ -1,6 +1,6 @@
 cask "upscayl" do
-  version "2.0.1"
-  sha256 "13ccea5608c1e5bf88035b6614b86b00b910b6d7567a99a236b01ac10b0ef9a1"
+  version "2.5.5"
+  sha256 "aa0e17ce93bff594191e07fa53d4ee2b0977973d8cb589f7363d78ef9bcd0593"
 
   url "https://github.com/upscayl/upscayl/releases/download/v#{version}/upscayl-#{version}-mac.dmg",
       verified: "github.com/upscayl/upscayl/"
