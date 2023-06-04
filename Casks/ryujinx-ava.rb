b9ff62695d58ad37a5fb50e2139deab90a2c00b1
Pavel Zwerschke <pavelzw@gmail.com>
2023-06-04 14:01:19 +0200

    ryujinx-ava 1.1.862 (new cask) (#13)

---
+++ Casks/ryujinx-ava.rb
@@ -0,0 +1,22 @@
+cask "ryujinx-ava" do
+  version "1.1.862"
+  sha256 "db7a3498cf1d4bf3bf36e9f7c8f454d634729870b57a854905cfa8cf519278be"
+
+  url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/test-ava-ryujinx-#{version}-macos_universal.app.tar.gz",
+      verified: "github.com/Ryujinx/release-channel-master/"
+  name "Ryujinx"
+  desc "Nintendo Switch emulator"
+  homepage "https://ryujinx.org/"
+
+  conflicts_with cask: "p-linnane/cask-unsigned/ryujinx"
+  depends_on macos: ">= :big_sur"
+
+  app "Ryujinx.app"
+
+  uninstall quit: "org.ryujinx.Ryujinx"
+
+  zap trash: [
+    "~/.config/Ryujinx",
+    "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
+  ]
+end
