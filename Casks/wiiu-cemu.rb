f367a4e4454f9b0c29be1c9899420ab39980c82e
Pavel Zwerschke <pavelzw@gmail.com>
2023-10-02 09:57:02 +0200

    wiiu-cemu 2.0-53 (#103)

--- Casks/wiiu-cemu.rb
+++ Casks/wiiu-cemu.rb
@@ -1,8 +1,8 @@
 cask "wiiu-cemu" do
   # this formula cannot be named cemu because it conflicts with the CEmu emulator for TI calculators
   # https://docs.brew.sh/How-to-Create-and-Maintain-a-Tap#naming
-  version "2.0-51"
-  sha256 "3926c02fed69fceb1e5e425a87bb9099e47630d48d6f2dee9b5d9debf280e93e"
+  version "2.0-53"
+  sha256 "7c972af1a9490a43f66d7a726c8154305f34230043ec5e8e5b5c0c3ed324eb88"
 
   url "https://github.com/cemu-project/Cemu/releases/download/v#{version}/cemu-#{version}-macos-12-x64.dmg",
       verified: "github.com/cemu-project/Cemu/"
